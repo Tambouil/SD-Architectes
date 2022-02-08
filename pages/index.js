a00faf221487717448166cc9024930ac760f9d29
Tambouil <60550907+Tambouil@users.noreply.github.com>
2022-02-08 19:05:15 +0100

    meta data

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>SD Architectes</title>
+        <title>
+          SD Architectes | Agence d'architecture à Rochefort sur mer
+        </title>
         <meta name="description" content="Agence d'architecture à Rochefort" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
