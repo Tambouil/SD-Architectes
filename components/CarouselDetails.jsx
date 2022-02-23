913bf355e3e26015d56c4bce8adae7642a5b944d
Valentin Berceaux <v.berceaux@gmail.com>
2022-02-24 00:25:56 +0100

    adding auto height to carousel details pages

--- components/CarouselDetails.jsx
+++ components/CarouselDetails.jsx
@@ -1,63 +1,39 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
-import { PrevButton, NextButton } from "./CarouselButtons";
-import useEmblaCarousel from "embla-carousel-react";
-import Autoplay from "embla-carousel-autoplay";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const CarouselDetails = ({ gallery, options = { loop: true } }) => {
-  const autoplay = useRef(
-    Autoplay(
-      { delay: 20000, stopOnInteraction: false },
-      (emblaRoot) => emblaRoot.parentElement
-    )
-  );
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-  const [emblaRef, emblaApi] = useEmblaCarousel(options, [autoplay.current]);
-  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
-  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+// import required modules
+import { Autoplay, Navigation, Pagination } from "swiper";
 
-  const scrollNext = useCallback(() => {
-    if (!emblaApi) return;
-    emblaApi.scrollNext();
-    autoplay.current.reset();
-  }, [emblaApi]);
-
-  const scrollPrev = useCallback(() => {
-    if (!emblaApi) return;
-    emblaApi.scrollPrev();
-    autoplay.current.reset();
-  }, [emblaApi]);
-
-  const onSelect = useCallback(() => {
-    if (!emblaApi) return;
-    setPrevBtnEnabled(emblaApi.canScrollPrev());
-    setNextBtnEnabled(emblaApi.canScrollNext());
-  }, [emblaApi]);
-
-  useEffect(() => {
-    if (!emblaApi) return;
-    onSelect();
-    emblaApi.on("select", onSelect);
-  }, [emblaApi, onSelect]);
-
+const CarouselDetails = ({ gallery }) => {
   return (
-    <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {gallery.fields.carousel.map((image) => (
-            <div className="embla__slide" key={image.sys.id}>
-              <div className="embla__slide__img">
-                <img
-                  src={"https:" + image.fields.file.url}
-                  alt="Vignette du projet"
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
-      <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
-    </div>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      autoHeight={true}
+      loop={true}
+      spaceBetween={20}
+      navigation={true}
+      modules={[Autoplay, Navigation]}
+      className="mySwiper"
+    >
+      {gallery.fields.carousel.map((image) => (
+        <SwiperSlide key={image.sys.id}>
+          <img
+            src={"https:" + image.fields.file.url}
+            alt="Vignette du projet"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
