6fba648ea07b6419540c0cac4f1641cbb2e68759
Valentin Berceaux <v.berceaux@gmail.com>
2023-07-09 17:34:09 +0200

    fix(contact): adresses break line

--- components/ContactInfos.jsx
+++ components/ContactInfos.jsx
@@ -1,25 +1,31 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+const options = {
+  renderText: (text) => {
+    return text.split('\n').reduce((children, textSegment, index) => {
+      return [...children, index > 0 && <br key={index} />, textSegment];
+    }, []);
+  },
+};
+
 const ContactInfos = ({ contact }) => {
   return (
     <>
       <div className="inner-content">
         <h1 className="title">{contact.fields.title}</h1>
-        <span className="text">{contact.fields.adress}</span>
-        <span className="email">
+        <div className="text">{documentToReactComponents(contact.fields.adress, options)}</div>
+        <p className="email">
           Email:
-          <a
-            href="mailto:agence@sdarchitectes.fr"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="mailto:agence@sdarchitectes.fr" target="_blank" rel="noreferrer">
             {contact.fields.email}
           </a>
-        </span>
+        </p>
       </div>
       <div className="inner-info">
         <p className="call">Téléphone</p>
-        <span className="call">
+        <p className="call">
           <a href="tel:0546995913">{contact.fields.phone}</a>
-        </span>
+        </p>
       </div>
     </>
   );
